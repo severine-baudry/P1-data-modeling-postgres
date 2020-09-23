5716edf1055b813d6cb9dbc8bab26aaba1e4ee20
severine-baudry <simone.yvonne.baudry@gmail.com>
2020-09-23 14:22:25 +0000

    Fix UPSERT in users

--- sql_queries.py
+++ sql_queries.py
@@ -95,7 +95,7 @@ user_table_insert = ("""
         INSERT INTO users
         (user_id, first_name, last_name, gender, level)
         VALUES( %(userId)s, %(firstName)s, %(lastName)s, %(gender)s, %(level)s )
-        ON CONFLICT (user_id) DO UPDATE SET level = %(userId)s;
+        ON CONFLICT (user_id) DO UPDATE SET level = %(level)s;
 """)
 
 song_table_insert = ("""
